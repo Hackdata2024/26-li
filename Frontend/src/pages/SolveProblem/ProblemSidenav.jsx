0acdb29f5030ae7d383e6bbedd99423a7a46ac24
Harsh Sharma <harshsharma20503@gmail.com>
2024-02-04 10:50:52 +0530

    added tabswitching flag

--- Frontend/src/pages/SolveProblem/ProblemSidenav.jsx
+++ Frontend/src/pages/SolveProblem/ProblemSidenav.jsx
@@ -71,23 +71,27 @@ function OffCanvasExample({ changeQuestionViaIndex, assignmentSolution, ...props
                     </Button>
                 </div>
                 <div style={{ width: "50%" }}>
-                    <Button className="submitAss" style={{ float: "right", marginRight: "4%",
-                                    font: "Fira Code",
-                                    paddingLeft: "20px", 
-                                    paddingRight: "20px",
-                                    color: "var(--sec)",
-                                    fontSize: "20px",
-                                    backgroundColor: "var(--high)",
-                                    borderColor: "var(--light)",
-                                    transition: "box-shadow 0.3s ease-in-out",                        
-                                }}
-                                    onMouseOver={(e) => {
-                                        e.target.style.color = "var(--bg1)";
-                                    }}
-                                    onMouseOut={(e) => {
-                                        e.target.style.boxShadow = "var(--sec)";
-                                    }}
-                                     onClick={handleSubmitAssignment}>
+                    <Button
+                        className="submitAss"
+                        style={{
+                            float: "right",
+                            marginRight: "4%",
+                            font: "Fira Code",
+                            paddingLeft: "20px",
+                            color: "var(--sec)",
+                            fontSize: "20px",
+                            backgroundColor: "var(--high)",
+                            borderColor: "var(--light)",
+                            transition: "box-shadow 0.3s ease-in-out",
+                        }}
+                        onMouseOver={(e) => {
+                            e.target.style.color = "var(--bg1)";
+                        }}
+                        onMouseOut={(e) => {
+                            e.target.style.boxShadow = "var(--sec)";
+                        }}
+                        onClick={handleSubmitAssignment}
+                    >
                         SUBMIT ASSIGNMENT
                     </Button>
                 </div>
